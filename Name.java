95c45a71f06da09bc5aa457199bfb9908ce0e88d
hjfhjfgjh <aydensantiagony@gmail.com>
2025-02-14 09:44:44 -0500

    grrg
    feregregre

---
+++ Name.java
@@ -0,0 +1,13 @@
+/************************************ 
+*Project: Demo for Comments 
+*Programmer: Ayden
+*Date: feburary,13, 2025
+*Program Name: Comments.java 
+*************************************/ 
+public class Name {
+    public static void main(String[]args){
+
+    System.out.println("NAME\t\tSCORE\npicard\t\t1874\nriker\t\t1789");
+    }
+
+}
