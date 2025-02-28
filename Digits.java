fdfee960da60dc4c75e0ec34127c663edafdedbe
hjfhjfgjh <aydensantiagony@gmail.com>
2025-02-28 09:54:24 -0500

    reghwrttth
    htrth

--- Digits.java
+++ Digits.java
@@ -10,20 +10,21 @@ Digits.java
 
 
 
-public Static main (String[]args){
+public static void main (String[]args){
 int number;
 int ones;
 int tens;
 int hundreds;
-Scanner input = new scanner(System.in);
+int thousands;
+Scanner input = new Scanner(System.in);
 
 System.out.println("Enter a 4 digit number")
 number =input.nextInt();
-/* number = 258; */
+number = 400;
 ones=number%10;
 tens=(number/10)%10;
 hundreds=(number/10)%10)%10;
-thousands=(number/10)/10)/10)%10);
+thousands=(((number/10)/10)/10)%10);
 System.out.println("your first digit is " + ones+"your second digit is"+ tens + "your third digit is" + hundreds+"your fourth digit is " + thousands);
 
 }
