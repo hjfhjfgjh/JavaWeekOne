4ea5fef0adabfe4b748ad305aea48e36b76cf0b6
hjfhjfgjh <aydensantiagony@gmail.com>
2025-03-03 10:09:48 -0500

    FWE
    FWEF

---
+++ tempconverter.java
@@ -0,0 +1,15 @@
+/*aydensantiago
+GradeAVG*/
+import java.util.*;
+
+public class tempconverter {
+    public static void main(String[]args){
+        Scanner input = new Scanner(System.in);
+        Double Fahreinheit;
+        double Celsius;
+        System.out.println("Enter a Fahrenheit temp");
+        Fahreinheit = input.nextDouble();
+        Celsius = 5 * (Fahreinheit - 32) / 9;
+        System.out.println("Your Celsius temp is " + Celsius);
+    }
+}
