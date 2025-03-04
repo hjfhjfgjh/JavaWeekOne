bfa45a5a46b49a46f12955a1f018bc5776da4c88
hjfhjfgjh <aydensantiagony@gmail.com>
2025-03-04 09:44:57 -0500

    grerg
    ge rhqerhg

---
+++ Divandmod.java
@@ -0,0 +1,30 @@
+/*ayden santiago*/
+public class DIVANDMOD {
+    import java.util.*;
+    public static void main(String[]args)
+    Scanner input = new Scanner(System.in);
+    int num1;
+    int num2;
+    int answer1;
+    int answer2;
+    int answer3;
+    int answer4;
+    System.out.println("Enter one number");
+    num1 = input.nextint();
+    System.out.println("Enter one number");
+    num2 = input.nextint();
+    System.out.println()
+    answer1 = num1/num2;
+    answer2 = num1%num2;
+    answer3 = num2/num1;
+    answer4 = num2%num1;
+     System.out.println(num1 + " divided by " + num2 + " is " + answer1 + "\n" + num1 + " modularly divided by " + num2 + " is " + answer2 + "\n" + num2 + " divided by " + num1 + " is " + answer3 + "\n" + num2 + " modularly divided by " + num1 + " is " + answer4);
+
+
+
+
+
+
+
+
+}
